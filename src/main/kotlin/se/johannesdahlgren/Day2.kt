de8457646a7f36c8247600902a4a6fd539684815
Johannes Dahlgren <johannes.dahlgren@gmail.com>
2022-12-02 17:40:55 +0100

    Day2, part2

--- src/main/kotlin/se/johannesdahlgren/Day2.kt
+++ src/main/kotlin/se/johannesdahlgren/Day2.kt
@@ -4,6 +4,33 @@ import se.johannesdahlgren.util.FileReader
 
 class Day2(private val filename: String) {
 
+    enum class Result {
+        WIN,
+        LOSE,
+        DRAW;
+
+        fun getMove(move: Move): Move {
+            return when (this) {
+                WIN -> move.looses()
+                LOSE -> move.beats()
+                DRAW -> move
+            }
+        }
+
+        companion object {
+            fun getResult(result: String): Result {
+                return when (result) {
+                    "X" -> LOSE
+                    "Y" -> DRAW
+                    "Z" -> WIN
+                    else -> {
+                        throw RuntimeException("Bad win input")
+                    }
+                }
+            }
+        }
+    }
+
     enum class Move {
         ROCK,
         PAPER,
@@ -16,7 +43,7 @@ class Day2(private val filename: String) {
                     "B", "Y" -> PAPER
                     "C", "Z" -> SCISSORS
                     else -> {
-                        throw RuntimeException("Bad input")
+                        throw RuntimeException("Bad move input")
                     }
                 }
             }
@@ -27,6 +54,14 @@ class Day2(private val filename: String) {
                 ROCK -> SCISSORS
                 PAPER -> ROCK
                 SCISSORS -> PAPER
+            }
+        }
+
+        fun looses(): Move {
+            return when (this) {
+                ROCK -> PAPER
+                PAPER -> SCISSORS
+                SCISSORS -> ROCK
             }
         }
 
@@ -48,6 +83,18 @@ class Day2(private val filename: String) {
         return FileReader.readFileLinesAsString(filename)
             .map { it.split(" ") }
             .map { Round(Move.getMove(it.last()), Move.getMove(it.first())) }
+            .sumOf { calculateScore(it) }
+    }
+
+    fun getTotalScoreFollowGuide(): Int {
+        return FileReader.readFileLinesAsString(filename)
+            .map { it.split(" ") }
+            .map {
+                Round(
+                    Result.getResult(it.last())
+                        .getMove(Move.getMove(it.first())), Move.getMove(it.first())
+                )
+            }
             .sumOf { calculateScore(it) }
     }
 
--- src/test/kotlin/se/johannesdahlgren/Day2Test.kt
+++ src/test/kotlin/se/johannesdahlgren/Day2Test.kt
@@ -17,4 +17,16 @@ class Day2Test {
         val day2 = Day2("day2")
         assertThat(day2.getTotalScore(), `is`(12855))
     }
+
+    @Test
+    fun part2Example() {
+        val day2 = Day2("day2Example")
+        assertThat(day2.getTotalScoreFollowGuide(), `is`(12))
+    }
+
+    @Test
+    fun part2() {
+        val day2 = Day2("day2")
+        assertThat(day2.getTotalScoreFollowGuide(), `is`(13726))
+    }
 }
