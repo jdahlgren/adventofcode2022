670ae5e67a363971f04e87ea75c3624a5965c378
Johannes Dahlgren <johannes.dahlgren@gmail.com>
2022-12-19 20:25:36 +0100

    Day18 part 1, refactor before part 2

--- src/main/kotlin/se/johannesdahlgren/aoc22/Day18.kt
+++ src/main/kotlin/se/johannesdahlgren/aoc22/Day18.kt
@@ -4,17 +4,20 @@ import se.johannesdahlgren.aoc22.util.FileReader
 import se.johannesdahlgren.aoc22.util.Pos3D
 
 class Day18(private val fileName: String) {
-    fun getSurfaceArea(): Int {
-        val lavaCubes = FileReader.readFileLinesAsString(fileName)
-            .map { it.split(",") }
-            .map { Pos3D(it[0].toInt(), it[1].toInt(), it[2].toInt()) }
+    private val sidesOfCube = 6
 
+    private fun get3dPosInput() = FileReader.readFileLinesAsString(fileName)
+        .map { it.split(",") }
+        .map { Pos3D(it[0].toInt(), it[1].toInt(), it[2].toInt()) }
+
+    fun getSurfaceArea(): Int {
+        val lavaCubes = get3dPosInput()
         return lavaCubes
             .sumOf { getSurfaceArea(it, lavaCubes) }
     }
 
     private fun getSurfaceArea(lavaCube: Pos3D, lavaCubes: List<Pos3D>): Int {
-        return 6 - lavaCubes
+        return sidesOfCube - lavaCubes
             .filter { lavaCube != it }
             .count { lavaCube.manhattanDistance(it) == 1 }
 
